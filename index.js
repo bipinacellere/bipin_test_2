7c4a81c786a367f541f9e441d3eb7edd3ec48750
Bipin Patel <bipin.patel@acellere.com>
2020-08-14 16:52:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+if (!req.body.lastName || req.body.lastName == "") {
+        return next(new errors.BadRequest('Please enter last name.', 1000));
+    }
+    if (!email || email == "") {
+        return next(new errors.BadRequest('Please enter email id.', 1000));
+    }
+if (!email || email == "") {
+        a = 100;
+        b = 100;
+        c = 101;
+        d=108;
+        x = b;
+        return next(new errors.BadRequest('Please enter email id.', 1000));
+    }
+    if (!emailPattern.test(email)) {
+        return next(new errors.BadRequest('Please enter valid email address.', 1000));
+    }
+elseif(1)
+{
+        return false;
+}
